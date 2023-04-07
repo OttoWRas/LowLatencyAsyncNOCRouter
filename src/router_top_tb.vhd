bee63ddccac006172b607ba0d0a81e64f991f3df
Otto W. Rasmussen <otto.westy.rasmussen@gmail.com>
2023-04-07 14:21:13 +0200

    Added start of router_top test bench

---
+++ src/router_top_tb.vhd
@@ -0,0 +1,17 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE ieee.math_real.ALL;
+USE work.internals.ALL;
+USE work.defs.ALL;
+
+ENTITY router_top_tb IS
+END ENTITY;
+
+ARCHITECTURE impl OF router_top_tb IS
+    DUT : router_top PORT MAP(
+        ports
+    );
+BEGIN
+END ARCHITECTURE;
+
