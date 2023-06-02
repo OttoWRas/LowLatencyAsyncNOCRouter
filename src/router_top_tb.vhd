de747f276e2ee1b5d1e55a854ca5367b588b3c9b
Otto W. Rasmussen <otto.westy.rasmussen@gmail.com>
2023-06-02 18:17:49 +0200

    Extra cmooit

--- src/router_top_tb.vhd
+++ src/router_top_tb.vhd
@@ -6,66 +6,86 @@ USE std.textio.ALL;
 USE work.internals.ALL;
 USE work.defs.ALL;
 USE work.router_top;
+USE work.dataTx;
+USE work.dataRx;
+
 ENTITY router_top_tb IS
 END ENTITY;
 
 ARCHITECTURE impl OF router_top_tb IS
 
+    type logic_arr_l is array (0 to 3) of logic_arr;
+    type data_arr_l is array (0 to 3) of data_arr;
+
     SIGNAL rst : STD_LOGIC;
-    SIGNAL req_in : logic_arr;
-    SIGNAL data_in : data_arr;
-    SIGNAL ack_out : logic_arr;
-    SIGNAL ack_in : logic_arr;
+    SIGNAL req_in : logic_arr_l := (others => (others => '0') );
+    SIGNAL data_in : data_arr_l := (others => (others => (others => '0')) );
+    SIGNAL data_out : data_arr_l := (others => (others => (others => '0')) );
+    SIGNAL ack_out : logic_arr_l := (others => (others => '0') );
+    SIGNAL ack_in : logic_arr_l := (others => (others => '0') );
+    SIGNAL req_out : logic_arr_l := (others => (others => '0') );
 
 BEGIN
 
-    DUT : ENTITY router_top PORT MAP(
-        rst => rst,
-        req_in => req_in,
-        data_in => data_in,
-        ack_out => ack_out,
-        ack_in => ack_in
+    gen_network : for i in 0 to 3 generate
+        DUT : ENTITY router_top 
+        generic map (
+            rx => to_unsigned(i mod 4, 4),
+            ry => to_unsigned(i / 2, 4)
+        )
+        PORT MAP(
+            rst => rst,
+            req_in => req_in(i),
+            data_in => data_in(i),
+            ack_out => ack_out(i),
+            ack_in => ack_in(i),
+            req_out => req_out(i),
+            data_out => data_out(i)
         );
+    end generate;
+    
+    TX : entity dataTx generic map (
+        file_name => "stimulus.txt"
+    ) port map (
+        rst, req_in(3)(0), ack_out(3)(0), data_in(3)(0)
+    );
 
-    rst <= '1', '0' AFTER 5 ns;
-    req_in(0) <= '0', '1' AFTER 25 ns;
-    req_in(1) <= '0';
-    req_in(2) <= '0';
-    req_in(3) <= '0';
-    req_in(4) <= '0';
+    RX : entity dataRX port map (
+        rst, req_out(1)(3), ack_in(1)(3), data_out(1)(3)
+    );
 
-    ack_out(0) <= '0';
-    ack_out(1) <= '0';
-    ack_out(2) <= '0';
-    ack_out(3) <= '0';
-    ack_out(4) <= '0';
+    req_in(3)(1) <=  req_out(1)(0);
+    data_in(3)(1) <= data_out(1)(0);
+    ack_in(3)(1) <= ack_out(1)(0);
 
-    --data_in(0) <= "0011001000001111";
-    data_in(1) <= "0010011000001111";
-    data_in(2) <= "0010011000001111";
-    data_in(3) <= "0100001000001111";
-    data_in(4) <= "0000001100001111";
+    req_in(1)(0) <=  req_out(3)(1);
+    data_in(1)(0) <= data_out(3)(1);
+    ack_in(1)(0) <= ack_out(3)(1);
 
-    PROC_SEQUENCER : PROCESS
-        FILE text_file : text OPEN read_mode IS "stimulus.txt";
-        VARIABLE ok : BOOLEAN;
-        VARIABLE ack : STD_LOGIC;
-        VARIABLE text_line : line;
-        VARIABLE wait_time : TIME;
-        VARIABLE data : bit_vector(DATA_WIDTH - 1 DOWNTO 0);
-    BEGIN
-        IF NOT endfile(text_file) THEN
-        --IF (ack_in(0) /= ack) THEN
-            readline(text_file, text_line);
-            read(text_line, wait_time);
-            read(text_line, data);
+    req_in(2)(2) <= req_out(0)(0);
+    data_in(2)(2) <= data_out(0)(0);
+    ack_in(2)(2) <= ack_out(0)(0);
 
-            IF text_line.ALL'length = 0 OR text_line.ALL(1) = '#' THEN
-            END IF;
-            data_in(0) <= To_StdLogicVector(data);
-            WAIT FOR wait_time;
-        --END IF;
-        WAIT FOR 2 ns;
-    END IF;
-END PROCESS;
+    req_in(0)(0) <= req_out(2)(2);
+    data_in(0)(0) <= data_out(2)(2);
+    ack_in(0)(0) <= ack_out(2)(2);
+
+    req_in(0)(1) <= req_out(2)(3);
+    data_in(0)(1) <= data_out(2)(3);
+    ack_in(0)(1) <= ack_out(2)(3);
+
+    req_in(2)(3) <= req_out(0)(1);
+    data_in(2)(3) <= data_out(0)(1);
+    ack_in(2)(3) <= ack_out(0)(1);
+
+    req_in(3)(3) <= req_out(1)(2);
+    data_in(3)(3) <= data_out(1)(2);
+    ack_in(3)(3) <= ack_out(1)(2);
+
+    req_in(1)(2) <= req_out(3)(3);
+    data_in(1)(2) <= data_out(3)(3);
+    ack_in(1)(2) <= ack_out(3)(3);
+
+    rst <= '1', '0' AFTER 5 ns, '1' AFTER 500 ns;
+
 END ARCHITECTURE;
